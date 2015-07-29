7d375eccbf888f7941dc25b162249f07e0261844
Adnard-kod <andra.lally@gmail.com>
2015-07-29 10:59:18 -0500

    push

--- js/main.js
+++ js/main.js
@@ -1,39 +1,39 @@
-var DATA = [
-  { "name": "Andra Lally",
-    "img": "http://lorempixel.com/500/200/fashion/1",
-    "address" : "some bio about this person above. Something interesting."
-  },
-  { "name": "Sam Pills",
-    "img": "http://lorempixel.com/500/200/fashion/2",
-    "address" : "some bio about this person above. Something interesting."
-  },
-  { "name": "Mark Smith",
-    "address" : "some bio about this person above. Something interesting.",
-    "img": "http://lorempixel.com/500/200/fashion/3"
-  },
-  { "name": "Harry Lulls",
-    "address" : "some bio about this person above. Something interesting.",
-    "img": "http://lorempixel.com/500/200/fashion/4"
-  },
-  { "name": "Heth Breg",
-    "address" : "some bio about this person above. Something interesting.",
-    "img": "http://lorempixel.com/500/200/fashion/5"
-  },
-  { "name": "Kathy Ponder",
-    "address" : "some bio about this person above. Something interesting.",
-    "img": "http://lorempixel.com/500/200/fashion/6"
-  }
-]
-
 var App = React.createClass({
-
-  render: function() {
+  getInitialState: function() {
+    return {
+      dataLoaded: false
+    };
+  },
+  loadData: function() {
+    $.ajax({
+      url: "data.json"
+    })
+    .success(function(data) {
+      this.people = data
+      this.setState({dataLoaded: true})
+    }.bind(this))
+    .error(function( xhr, error ) {
+      console.log(error);
+    }.bind(this));
+  },
+  displayViews: function() {
     return (
       <div>
         <Search />
-        < PersonList data={DATA} />
+        < PersonList data={this.people} />
       </div>
     );
+  },
+  render: function() {
+    this.loadData()
+    if (this.state.dataLoaded) {
+      return this.displayViews()
+    } else {
+      return (
+        <div/>
+      )
+    }
+
   }
 });
 
@@ -76,9 +76,9 @@ var Person = React.createClass({
         <div className="image-box">
           <img src={this.props.img} />
         </div>
-          <h2>{this.props.name}</h2>
-          <div className="body">
-            {this.props.address}
+          <div className="content">
+            <h2>{this.props.name}</h2>
+            <p>{this.props.address}</p>
           </div>
         </li>
       </div>
